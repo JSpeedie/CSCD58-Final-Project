82f9ad5ca49bb4c5c0d86e2e3a2fc2d165864437
JSpeedie <julianspeedie@gmail.com>
2024-01-29 12:34:11 -0500

    Added defined values to make code more readable

--- src/ftputil.h
+++ src/ftputil.h
@@ -4,6 +4,11 @@
 #define TRUE 1
 #define FALSE 0
 
+#define CMD_LS 1
+#define CMD_GET 2
+#define CMD_PUT 3
+#define CMD_QUIT 4
+
 #define FD_SETS(fds, rdset, n, i)\
   do {\
     for (i = 0; i < n; i++) { FD_SET(fds[i], rdset); }\
