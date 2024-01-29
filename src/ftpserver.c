c670e36d47c3ade27cc4c695bad6f07bbdeaa359
JSpeedie <julianspeedie@gmail.com>
2024-01-29 18:45:04 -0500

    Began cleanup on ftpserver.c

--- src/ftpserver.c
+++ src/ftpserver.c
@@ -1,165 +1,153 @@
-#include "comp.h"
-#include "enc.h"
-#include "ftputil.h"
-#include "pec-ftp.h"
-
 #include <arpa/inet.h>
 #include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/types.h>
-#include <time.h>
 #include <time.h>
 #include <unistd.h>
-#include <dirent.h>
 
-//function trims leading and trailing whitespaces
-void trim(char *str)
-{
+#include "comp.h"
+#include "enc.h"
+#include "ftputil.h"
+#include "pec-ftp.h"
 
-	int i;
-    int begin = 0;
 
-    int end = strlen(str) - 1;
+int read_port_command(char *str, char *client_ip, uint16_t *client_port) {
+	/* Read the port command, as specified at page 28 of:
+	 * https://www.ietf.org/rfc/rfc959.txt */
 
-    while (isspace((unsigned char) str[begin]))
-        begin++;
-
-    while ((end >= begin) && isspace((unsigned char) str[end]))
-        end--;
-
-    // Shift all characters back to the start of the string array.
-    for (i = begin; i <= end; i++)
-        str[i - begin] = str[i];
-
-    str[i - begin] = '\0'; // Null terminate string.
-}
-
-int get_client_ip_port(char *str, char *client_ip, int *client_port){
-	char *n1, *n2, *n3, *n4, *n5, *n6;
-	int x5, x6;
+	char *h1, *h2, *h3, *h4, *p1, *p2;
+	uint16_t p1_i, p2_i;
 
 	strtok(str, " ");
-	n1 = strtok(NULL, ",");
-	n2 = strtok(NULL, ",");
-	n3 = strtok(NULL, ",");
-	n4 = strtok(NULL, ",");
-	n5 = strtok(NULL, ",");
-	n6 = strtok(NULL, ",");
+	h1 = strtok(NULL, ",");
+	h2 = strtok(NULL, ",");
+	h3 = strtok(NULL, ",");
+	h4 = strtok(NULL, ",");
+	p1 = strtok(NULL, ",");
+	p2 = strtok(NULL, ",");
 
-	sprintf(client_ip, "%s.%s.%s.%s", n1, n2, n3, n4);
+	sprintf(client_ip, "%s.%s.%s.%s", h1, h2, h3, h4);
 
-	x5 = atoi(n5);
-	x6 = atoi(n6);
-	*client_port = (256*x5)+x6;
+	p1_i = atoi(p1);
+	p2_i = atoi(p2);
+	(*client_port) = (p1_i << 8) + p2_i;
 
-	printf("client_ip: %s client_port: %d\n", client_ip, *client_port);
+	return 0;
+}
+
+int setup_data_connection(int *fd, char *client_ip, int client_port, int server_port) {
+	
+	struct sockaddr_in client_addr, temp_addr;
+
+	if ( ( (*fd) = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("socket error");
+		return -1;
+	}
+
+	//bind port for data connection to be server port - 1 by using a temporary
+	//struct sockaddr_in
+	bzero(&temp_addr, sizeof(temp_addr));
+	temp_addr.sin_family = AF_INET;
+	temp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/* Setup data connection to have a port that is the control connection
+	 * port - 1, as specified at page 18 of:
+	 * https://www.ietf.org/rfc/rfc959.txt */
+	temp_addr.sin_port = htons(server_port - 1);
+
+	while (bind( (*fd), (struct sockaddr*) &temp_addr, sizeof(temp_addr)) < 0) {
+		server_port--;
+		temp_addr.sin_port = htons(server_port);
+	}
+
+	/* Initiate data connection with client */
+	bzero(&client_addr, sizeof(client_addr));
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_port = htons(client_port);
+	if (inet_pton(AF_INET, client_ip, &client_addr.sin_addr) <= 0) {
+		perror("inet_pton error");
+		return -1;
+	}
+
+	if (connect(*fd, (struct sockaddr *) &client_addr, sizeof(client_addr)) < 0) {
+		perror("connect error");
+		return -1;
+	}
+
 	return 1;
 }
 
-int setup_data_connection(int *fd, char *client_ip, int client_port, int server_port){
-	
-    struct sockaddr_in cliaddr, tempaddr;
 
-    if ( (*fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    	perror("socket error");
-    	return -1;
-    }
+int get_filename(char *input, char *fileptr) {
 
-    //bind port for data connection to be server port - 1 by using a temporary struct sockaddr_in
-    bzero(&tempaddr, sizeof(tempaddr));
-    tempaddr.sin_family = AF_INET;
-    tempaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    tempaddr.sin_port   = htons(server_port-1);
-
-    while((bind(*fd, (struct sockaddr*) &tempaddr, sizeof(tempaddr))) < 0){
-    	//perror("bind error");
-        server_port--;
-    	tempaddr.sin_port   = htons(server_port);
-    }
-
-
-    //initiate data connection fd with client ip and client port             
-    bzero(&cliaddr, sizeof(cliaddr));
-    cliaddr.sin_family = AF_INET;
-    cliaddr.sin_port   = htons(client_port);
-    if (inet_pton(AF_INET, client_ip, &cliaddr.sin_addr) <= 0){
-    	perror("inet_pton error");
-    	return -1;
-    }
-
-    if (connect(*fd, (struct sockaddr *) &cliaddr, sizeof(cliaddr)) < 0){
-    	perror("connect error");
-    	return -1;
-    }
-
-    return 1;
+	char *filename = NULL;
+	filename = strtok(input, " ");
+	filename = strtok(NULL, " ");
+	if (filename == NULL) {
+		return -1;
+	} else {
+		strncpy(fileptr, filename, strlen(filename));
+		return 1;
+	}
 }
 
-int get_filename(char *input, char *fileptr){
 
-    char *filename = NULL;
-    filename = strtok(input, " ");
-    filename = strtok(NULL, " ");
-    if(filename == NULL){
-        return -1;
-    }else{
-    	strncpy(fileptr, filename, strlen(filename));
-        return 1;
-    }
-}
-
-int get_command(char *command){
+int get_command(char *command) {
 	char cpy[1024];
 	strcpy(cpy, command);
 	char *str = strtok(cpy, " ");
-	int value;
+	int value = 0;
 
-	//populated value valriable to indicate back to main which input was entered
-    if(strcmp(str, "LIST") == 0){value = 1;}
-    else if(strcmp(str, "RETR") == 0){value = 2;}
-    else if(strcmp(str, "STOR") == 0){value = 3;}
+	//populated value variable to indicate back to main which input was entered
+    if(strcmp(str, "LIST") == 0){value = CMD_LS;}
+    else if(strcmp(str, "RETR") == 0){value = CMD_GET;}
+    else if(strcmp(str, "STOR") == 0){value = CMD_PUT;}
     else if(strcmp(str, "SKIP") == 0){value = 4;}
     else if(strcmp(str, "ABOR") == 0){value = 5;}
 
     return value;
 }
 
+
+
+/* CSCD58 addition - Encryption + Parallelization */
 int do_dh(int controlfd, int datafd, uint32_t key[4]) {
-    uint64_t dh_p = 1;
-    dh_p = (dh_p << 32) - 99;
-    uint64_t dh_g = 5;
-    uint64_t dh_b, dh_ka, dh_kb, dh_k;
-    fd_set fds;
-    FD_ZERO(&fds);
+	uint64_t dh_p = 1;
+	dh_p = (dh_p << 32) - 99;
+	uint64_t dh_g = 5;
+	uint64_t dh_b, dh_ka, dh_kb, dh_k;
+	fd_set fds;
+	FD_ZERO(&fds);
 
-    for (int i = 0; i < 4; i++) {
-        dh_b = (rand() % (dh_p - 2)) + 2;
-        dh_kb = sq_mp(dh_g, dh_b, dh_p);
-        FD_SET(datafd, &fds);
+	for (int i = 0; i < 4; i++) {
+		dh_b = (rand() % (dh_p - 2)) + 2;
+		dh_kb = sq_mp(dh_g, dh_b, dh_p);
+		FD_SET(datafd, &fds);
 
-        select(datafd + 1, &fds, NULL, NULL, NULL);
-        read(datafd, (char *)&dh_ka, sizeof(dh_ka));
-        
-        write(datafd, (char *)&dh_kb, sizeof(dh_kb));
+		select(datafd + 1, &fds, NULL, NULL, NULL);
+		read(datafd, (char *)&dh_ka, sizeof(dh_ka));
+		
+		write(datafd, (char *)&dh_kb, sizeof(dh_kb));
 
-        dh_k = sq_mp(dh_ka, dh_b, dh_p);
-        key[i] = dh_k & 0xffffffff;
-    }
-    
-    return 0;
+		dh_k = sq_mp(dh_ka, dh_b, dh_p);
+		key[i] = dh_k & 0xffffffff;
+	}
+	
+	return 0;
 }
+/* CSCD58 end of addition - Encryption + Parallelization */
+
 
 int do_list(int controlfd, int datafd, char *input){
 	char filelist[1024], sendline[MAXLINE+1], str[MAXLINE+1];
@@ -212,6 +200,7 @@ int do_list(int controlfd, int datafd, char *input){
     return 1;
 }
 
+
 int do_retr(int controlfd, int *datafds, char *input){
 	char filename[1024], sendline[MAXLINE+1], str[MAXLINE+1];
 	bzero(filename, (int)sizeof(filename));
@@ -238,7 +227,7 @@ int do_retr(int controlfd, int *datafds, char *input){
 		return -1;
 	}
 
-	/* CSCD58 addition */
+	/* CSCD58 addition - Compression */
 	char * encsuffix = ".enc";
 	int encsuffix_len = strlen(encsuffix);
 	char * compsuffix = ".comp";
@@ -274,7 +263,7 @@ int do_retr(int controlfd, int *datafds, char *input){
 	enc_file(str, encoutputfilepath, key);
 
 	sprintf(str, "cat %s", encoutputfilepath);
-	/* CSCD58 end of addition */
+	/* CSCD58 end of addition - Compression */
 
 	FILE *in;
 	extern FILE *popen();
@@ -291,7 +280,7 @@ int do_retr(int controlfd, int *datafds, char *input){
                 maxfdp1 = datafds[i] + 1;
         }
 
-	/* CSCD58 addition */
+	/* CSCD58 addition - Parallelization */
 	uint16_t nmem_read = 0;
 	int j = 0;
 	uint32_t cur_pos = 0;
@@ -324,12 +313,12 @@ int do_retr(int controlfd, int *datafds, char *input){
         
         cur_pos += nmem_read;
 	}
-	/* CSCD58 end of addition */
+	/* CSCD58 end of addition - Parallelization */
 
 	sprintf(sendline, "200 Command OK");
 	write(controlfd, sendline, strlen(sendline));
 	pclose(in);
-	/* CSCD58 addition */
+	/* CSCD58 addition - Compression */
 	if (KEEP_TEMP_ENC_FILES != 1) {
 		if (0 != remove(encoutputfilepath)) {
 			fprintf(stderr, "WARNING: could not remove temporary encrypted .enc file!\n");
@@ -340,9 +329,10 @@ int do_retr(int controlfd, int *datafds, char *input){
 			fprintf(stderr, "WARNING: could not remove temporary compressed .comp file!\n");
 		} 
 	}
-	/* CSCD58 end of addition */
+	/* CSCD58 end of addition - Compression */
 	return 1;
 }
+
 
 int do_stor(int controlfd, int datafd, char *input){
 	char filename[1024], sendline[MAXLINE+1], recvline[MAXLINE+1], str[MAXLINE+1], temp1[1024];
@@ -365,7 +355,7 @@ int do_stor(int controlfd, int datafd, char *input){
 		return -1;
 	}
 
-	/* CSCD58 addition */
+	/* CSCD58 addition - Compression */
 	/* Make temporary <filename>.comp.enc-XXXXXX file for receiving the file */
 	char * encsuffix = ".enc";
 	int encsuffix_len = strlen(encsuffix);
@@ -383,8 +373,7 @@ int do_stor(int controlfd, int datafd, char *input){
 	close(r);
 	unlink(recvfilepath);
 	sprintf(temp1, "%s", recvfilepath);
-	/* sprintf(temp1, "%s-out", filename); */
-	/* CSCD58 end of addition */
+	/* CSCD58 end of addition - Compression*/
 
 	FILE *fp;
 	if((fp = fopen(temp1, "w")) == NULL){
@@ -404,7 +393,7 @@ int do_stor(int controlfd, int datafd, char *input){
 	write(controlfd, sendline, strlen(sendline));
 	fclose(fp);
 
-	/* CSCD58 addition */
+	/* CSCD58 addition - Compression */
 	/* Create 'cat <filename>.comp.enc-XXXXXX' */
 	int unencinputfilelen = strlen(temp1) + 5;
 	char unencinputfilepath[unencinputfilelen];
@@ -447,100 +436,134 @@ int do_stor(int controlfd, int datafd, char *input){
 			fprintf(stderr, "WARNING: could not remove temporary compressed .comp file!\n");
 		}
 	}
-	/* CSCD58 end of addition */
+	/* CSCD58 end of addition - Compression */
 
 	return 1;
 }
 
-int main(int argc, char **argv){
 
-	int	listenfd, connfd, port;
-	struct sockaddr_in	servaddr;
+int main(int argc, char **argv) {
+	int listenfd, connfd, port;
+	struct sockaddr_in servaddr;
 	pid_t pid;
 
-	if(argc != 2){
+	if (argc != 2) {
 		printf("Invalid Number of Arguments...\n");
 		printf("Usage: ./ftpserver <listen-port>\n");
 		exit(-1);
 	}
 
+	/* Parse server port from commandline args */
 	sscanf(argv[1], "%d", &port);
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if ( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("socket error");
+		exit(-1);
+	}
 
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
+	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(port);
+	servaddr.sin_port = htons(port);
 
-	bind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
+	if (bind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0) {
+		perror("bind error");
+		exit(-1);
+	}
 
-	listen(listenfd, LISTENQ);
+	if (listen(listenfd, LISTENQ) < 0) {
+		perror("listen error");
+		exit(-1);
+	}
 
-	while(1){
-		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-		printf("New Client Detected...\n");
-		//child process---------------------------------------------------------------
-		if((pid = fork()) == 0){
-			close(listenfd);
+	struct sockaddr_in address;
+	socklen_t addrlen = sizeof(address);
 
-			int datafds[NDATAFD], code, x = 0, client_port = 0;
-			char recvline[MAXLINE+1];
-			char client_ip[50], command[4096];
+	while (1) {
+		if ( (connfd = accept(listenfd, (struct sockaddr *) &address, &addrlen)) < 0) {
+			/* New client did NOT connect successfully */
+			perror("accept error");
+		/* New client connected successfully */
+		} else {
+			// TODO: not sure this prints the right address/port
+			fprintf(stdout, "Notice: Received a new client %s:%d!\n", \
+				inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+			pid = fork();
+			if (pid < 0) {
+				perror("fork error");
+			/* If child process... */
+			//child process---------------------------------------------------------------
+			} else if (pid == 0) {
+				close(listenfd);
 
-			while(1){
-				bzero(recvline, (int)sizeof(recvline));
-				bzero(command, (int)sizeof(command));
+				int datafds[NDATAFD], cmd, x = 0;
+				uint16_t client_port = 0;
+				char recvline[MAXLINE+1];
+				char client_ip[INET_ADDRSTRLEN], command[4096];
 
-				//get client's data connection port
-    			if((x = read(connfd, recvline, MAXLINE)) < 0){
-    				break;
-    			}
-    			printf("*****************\n%s \n", recvline);
-                if(strcmp(recvline, "QUIT") == 0){
-                    printf("Quitting...\n");
-                    char goodbye[1024];
-                    sprintf(goodbye,"221 Goodbye");
-                    write(connfd, goodbye, strlen(goodbye));
-                    close(connfd);
-                    break;
-                }
-    			get_client_ip_port(recvline, client_ip, &client_port);
+				while (1) {
+					bzero(recvline, (int)sizeof(recvline));
+					bzero(command, (int)sizeof(command));
 
-    			int i;
-                while (i < NDATAFD) {
-                    if((setup_data_connection(datafds + i, client_ip, client_port, port)) < 0){
-                            break;
-                    }
-                    i++;
-                }
+					//get client's data connection port
+					if ((x = read(connfd, recvline, MAXLINE)) < 0){
+						break;
+					}
 
-    			if((x = read(connfd, command, MAXLINE)) < 0){
-    				break;
-    			}
+					printf("(%d) *****************\n", getpid());
+					printf("(%d) %s\n", getpid(), recvline);
 
-    			printf("-----------------\n%s \n", command);
+					// TODO: this should be a strncmp
+					if(strcmp(recvline, "QUIT") == 0){
+						printf("(%d) Quitting...\n", getpid());
+						// TODO: this buffer is oversized
+						char goodbye[1024];
+						sprintf(goodbye,"221 Goodbye");
+						write(connfd, goodbye, strlen(goodbye));
+						close(connfd);
+						break;
+					}
 
-    			code = get_command(command);
-    			if(code == 1){
-    				do_list(connfd, datafds[0], command);
-    			}else if(code == 2){
-    				do_retr(connfd, datafds, command);
-    			}else if(code == 3){
-    				do_stor(connfd, datafds[0], command);
-    			}else if(code == 4){
-                    char reply[1024];
-                    sprintf(reply, "550 Filename Does Not Exist");
-                    write(connfd, reply, strlen(reply));
-                }
-                i = 0;
-    			close_data_connections(datafds);
+					read_port_command(recvline, &client_ip[0], &client_port);
+					printf("(%d) client_ip: %s client_port: %d\n", getpid(), client_ip, client_port);
+
+					int i;
+					while (i < NDATAFD) {
+						if ((setup_data_connection(datafds + i, client_ip, client_port, port)) < 0) {
+							break;
+						}
+						i++;
+					}
+
+					// TODO: what does this read accomplish? it seems important
+					if ((x = read(connfd, command, MAXLINE)) < 0) {
+						break;
+					}
+
+					printf("(%d) -----------------\n%s \n", getpid(), command);
+
+					cmd = get_command(command);
+					if(cmd == CMD_LS){
+						do_list(connfd, datafds[0], command);
+					}else if(cmd == CMD_GET){
+						do_retr(connfd, datafds, command);
+					}else if(cmd == CMD_PUT){
+						do_stor(connfd, datafds[0], command);
+					}else if(cmd == 4){
+						char reply[1024];
+						sprintf(reply, "550 Filename Does Not Exist");
+						write(connfd, reply, strlen(reply));
+					}
+					i = 0;
+					close_data_connections(datafds);
+				}
+				// TODO: this print should almost certainly be removed
+				printf("(%d) Exiting Child Process...\n", getpid());
+				close(connfd);
+				_exit(1);
 			}
-    		printf("Exiting Child Process...\n");
-    		close(connfd);
-    		_exit(1);
+			//end child process-------------------------------------------------------------
+			close(connfd);
 		}
-		//end child process-------------------------------------------------------------
-		close(connfd);
 	}
 }
